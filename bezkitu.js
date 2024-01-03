b1d50e58d69de0a87dc020c1403c5403820895d8
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2024-01-03 07:45:06 +0100

    Add files via upload

---
+++ bezkitu.js
@@ -0,0 +1,5 @@
+var total = 131 // number
+total = "This is JavaScript"; //string
+total= {"Customer": Karen McGrath"}; //object
+total = function ({alert("Hello");} //function
+total(); // => Hello (executes function
