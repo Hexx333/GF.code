c1ae8f5314149498c8bce1c46b8e71b8da8119f9
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2024-07-22 09:05:13 +0200

    Delete bezkitu.js

--- bezkitu.js
+++
@@ -1,5 +0,0 @@
-var total = 131 // number
-total = "This is JavaScript"; //string
-total= {"Customer": Bartosz Wesołowski"}; //object
-total = function ({alert("Hello my account manager number ;93160014621891267050167387 //function
-total(); // => Hello (executes function
